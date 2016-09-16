199338f238e1096d44dff8cf6b3dca130b9522c9
meenuganesh91 <MeenuVimal@Meenus-MacBook-Pro.local>
2016-09-16 08:46:02 -0700

    BestCPU.c

--- BestCPU.c
+++ BestCPU.c
@@ -1,101 +1,167 @@
 //
 //  BestCPU.c
-//  
 //
-//  Created by Vishnu, Priya on 9/15/16.
-// 16 bit architecture
+// 32 bit architecture
 //
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-/*int* convertBinary(int decimalNumber){
-    int stroreBinaryArray[16] = {0};
+int MEMORY[65536] = {0};   // Memory
+
+/*char* convertBinary(int decimalNumber){
+    printf("Hello\n");
+    char stroreBinaryArray[17] = {0};
     int i = 0;
     while(decimalNumber != 1){
-        stroreBinaryArray[i] = (decimalNumber % 2);
+        char dec;
+        dec = (decimalNumber%2) + '0';
+        printf("Hello again %c   %d\n", dec, decimalNumber%2);
+        stroreBinaryArray[i] = dec;
         i++;
+        decimalNumber = decimalNumber/2;
     }
+    printf("NUM: %s\n", stroreBinaryArray);
     return stroreBinaryArray;
 }*/
-//
-//int MEMORY[65536] = {0} ;
-//
-//int loadWord(char destinationRegister, char offset, char baseAddressRegister, char* MAR, char* MDR){
-//    int tempLocal = atoi(offset) + atoi(baseAddressRegister);
-//    destinationRegister = tempLocal
-//    
-//}
-//
-//int storeWord(int data, int offset, int baseAddress, int * MAR, int* MDR){
-//
-//}
 
 
+/* int loadWord(char destinationRegister, char offset, char baseAddressRegister, char* MAR, char* MDR){
+    int tempLocal = atoi(offset) + atoi(baseAddressRegister);
+    destinationRegister = tempLocal
+ 
+}*/
 
-int main()
+
+/* int storeWord(int data, int offset, int baseAddress, int * MAR, int* MDR){
+}*/
+
+int decimalToBinary(int n) {
+    int rem;
+    int binary = 0;
+    int i = 1;
+    
+    while(n != 0) {
+        rem = n%2;
+        n = n/2;
+        binary= binary + (rem*i);
+        i = i*10;
+    }
+    return binary;
+}
+
+char* decToBin(int n){
+    printf("Num %d", n);
+    char binary[16] = {0};
+    int rem, i = 15;
+    while(n != 0){
+        rem = n%2;
+        n = n/2;
+        //printf("rem + 0 %c", (rem+'0'));
+        binary[i] = rem + '0';
+        i--;
+        
+    }
+    printf("binary: %s\n", binary);
+    return binary;
+}
+
+
+//Function for storing the instruction to the memory
+void storeToMemory(char *inst){
+    
+    char *split = (char *) malloc(16);
+    char * str = (char *) malloc(16);
+    strcpy(str,inst);
+    
+    char code[33] = "";
+    
+    int argNum = 0;
+    split = strtok(str, " ,.-");
+    printf("Split: %s\n", split);
+    while(split != NULL){
+        if(argNum == 0){                    //opcode parsing
+            if(strcmp(split, "FETCH")==0){
+                strcat(code, "00000");
+            }
+            else if(strcmp(split, "STORE")==0){
+                strcat(code, "00001");
+            }
+            argNum++;
+            printf("code: %s %lu\n", code, sizeof(code));
+        }
+        
+        else if(argNum == 1){              //register
+            printf("Split: %s\n", split);
+            int reg = split[1] - '0';
+            int bin = decimalToBinary(reg);
+            char binary[5];
+            sprintf(binary, "%d", bin);
+            
+            int len = 10 - strlen(binary);
+            memmove(binary+len, binary, strlen(binary));
+            for ( int i = 0; i < len; i++ )
+                binary[i] = '0';
+            
+            strcat(code, binary);
+            printf("code: %s %lu\n", code, sizeof(code));
+            argNum++;
+        }
+        
+        else if(argNum == 2){
+            printf("Split: %s\n", split);
+            int addr = atoi(split);
+            printf("Addr: %d\n", addr);
+            //  "1100110011111111"
+            //long binAddr = decimalToBinary(addr);
+            //printf("binAddr %lu", binAddr);
+            char memAddr[17];
+            strcpy(memAddr,decToBin(addr));
+            
+            /*int len = 16 - strlen(memAddr);
+            memmove(memAddr+len, memAddr, strlen(memAddr));
+            for ( int i = 0; i < len; i++ )
+                memAddr[i] = '0';
+            */
+            printf("MemAddr: %s\n", memAddr);
+            strcat(code, memAddr);
+            printf("code: %s %lu\n", code, sizeof(code));
+            argNum++;
+            
+        }
+        split = strtok(NULL, " ,.-");
+        
+    }
+    return;
+    
+    
+}
+
+
+int main(int argc, char **argv)
 
 {
     // General Purpose registers r0-r7
-    //int i;
-    //int* binary
-    
-    int  r0 ;
-    int  r1 ;
-    int  r2 ;
-    int  r3 ;
-    int  r4 ;
-    int  r5 ;
-    int  r6 ;
-    int  r7 ;
+    int  r0, r1, r2, r3, r4, r5, r6, r7;
 
-    int PC ;                    // Program Counter
+    int PC;                    // Program Counter
+    int MAR;                   // Memory Address
+    int MDR;                   // Memory Data Register
+    int FLG;                   // Flag register
+    int SP;                    // Stack Pointer
+    int RA;                    // Return Address register
+    const int ZERO = 0;        // Zero Address Register
     
-    int MAR ;                   // Memory Address
+    for(int i=1; i< argc; i++) {
+        printf("%s\n", argv[i]);
+        storeToMemory(argv[i]);
+        //execute(argv[i]);
+    }
     
-    int MDR ;                   // Memory Data Register
-    
-    int FLG ;                   // Flag register
-    
-    int SP ;                    // Stack Pointer
-    
-    int RA ;                    // Return Address register
-    
-    const int ZERO = 0;         // Zero Address Register
-    
-    int MEMORY[65536] = {0} ;   // Memory
 
     //int binaryArray = convertBinary(10);
-    
-    //r1 = {0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0} ;
-    //r1 = {{0}, {1}};
-//    
-//    char temp[] = "0000000000001010" ;
-//    
-//    strcpy(r1,"110011000000100") ;
-//    
-//    printf ("%s", r1) ;
-    
-//    for (i= 0; i < sizeof(temp); i++) {
-//        r1[i] = temp[i];
-//        printf ("%c",r1[i]);
-//    }
-//    
-//    printf ("\n");
-//    
-//    char temp[] = "0000000000001010" ;
-//    
-//    for (i= 0; i < sizeof(temp); i++) {
-//        r1[i] = temp[i];
-//        printf ("%c",r1[i]);
-//    }
-//    
-//    printf ("\n");
-//    
-//    
-//    printf("r1[0] = %c", r1[0]) ;
-//  binary = convertBinary(10);
-
-    
+ 
     return 0 ;
 }

312284df57ff246c3c307ec16728fb67d6e7e0b9
meenuganesh91 <MeenuVimal@Meenus-MacBook-Pro.local>
2016-09-16 17:10:48 -0700

    Added the code for formatting string to store into memory

--- BestCPU.c
+++ BestCPU.c
@@ -47,7 +47,7 @@ int MEMORY[65536] = {0};   // Memory
 
 /* int storeWord(int data, int offset, int baseAddress, int * MAR, int* MDR){
 }*/
-
+//Function to convert decimal to binary
 int decimalToBinary(int n) {
     int rem;
     int binary = 0;
@@ -62,31 +62,53 @@ int decimalToBinary(int n) {
     return binary;
 }
 
+//Function to convert large decimal numbers to binary. Answer returned in a char string
 char* decToBin(int n){
-    printf("Num %d", n);
-    char binary[16] = {0};
-    int rem, i = 15;
+    printf("Num %d\n", n);
+    char *binary = (char*) malloc(16);
+    int rem, i = 0;
     while(n != 0){
         rem = n%2;
         n = n/2;
-        //printf("rem + 0 %c", (rem+'0'));
         binary[i] = rem + '0';
-        i--;
+        printf("Rem: %d    Num: %d    binary[i]: %c\n", rem, n, binary[i]);
+        i++;
         
     }
+    binary[i] = '\0';
+    printf("binary before reversal: %s\n", binary);
+
+    //Reverse string
+    char *start = binary;
+    char *end = start + strlen(binary) - 1; /* -1 for \0 */
+    char temp;
+    
+    /* reverse */
+    while (end > start)
+    {
+        /* swap */
+        temp = *start;
+        *start = *end;
+        *end = temp;
+        
+        /* move */
+        ++start;
+        --end;
+    }
+
     printf("binary: %s\n", binary);
     return binary;
 }
 
 
 //Function for storing the instruction to the memory
-/*void storeToMemory(char *inst){
+void storeToMemory(char *inst){
     
     char *split = (char *) malloc(16);
     char * str = (char *) malloc(16);
     strcpy(str,inst);
     
-    char code[33] = "";
+    char code[34] = "";
     
     int argNum = 0;
     split = strtok(str, " ,.-");
@@ -94,10 +116,10 @@ char* decToBin(int n){
     while(split != NULL){
         if(argNum == 0){                    //opcode parsing
             if(strcmp(split, "FETCH")==0){
-                strcat(code, "00000");
+                strcat(code, "0000");
             }
             else if(strcmp(split, "STORE")==0){
-                strcat(code, "00001");
+                strcat(code, "0001");
             }
             argNum++;
             printf("code: %s %lu\n", code, sizeof(code));
@@ -110,7 +132,7 @@ char* decToBin(int n){
             char binary[5];
             sprintf(binary, "%d", bin);
             
-            int len = 10 - strlen(binary);
+            int len = 12 - strlen(binary);
             memmove(binary+len, binary, strlen(binary));
             for ( int i = 0; i < len; i++ )
                 binary[i] = '0';
@@ -120,22 +142,20 @@ char* decToBin(int n){
             argNum++;
         }
         
-        else if(argNum == 2){
+        else if(argNum == 2){           //memory
             printf("Split: %s\n", split);
             int addr = atoi(split);
             printf("Addr: %d\n", addr);
-            //  "1100110011111111"
-            //long binAddr = decimalToBinary(addr);
-            //printf("binAddr %lu", binAddr);
-            char memAddr[17];
+            //char memAddr[17];
+            char *memAddr = (char*) malloc(18);
             strcpy(memAddr,decToBin(addr));
             
-            /*int len = 16 - strlen(memAddr);
+            int len = 16 - strlen(memAddr);
             memmove(memAddr+len, memAddr, strlen(memAddr));
             for ( int i = 0; i < len; i++ )
                 memAddr[i] = '0';
             
-            printf("MemAddr: %s\n", memAddr);
+            printf("MemAddr:%s\n", memAddr);
             strcat(code, memAddr);
             printf("code: %s %lu\n", code, sizeof(code));
             argNum++;
@@ -147,7 +167,7 @@ char* decToBin(int n){
     return;
     
     
-}*/
+}
 
 void execute(char *inst[]){                         // -TARSHITH
 	char *STORE = "STORE";
@@ -190,8 +210,8 @@ int main(int argc, char *argv[])    //CHANGED FROM **ARGV TO *ARGV[] - TARSHITH
     
     
     for(int i=1; i< argc; i++) {
-        //printf("%s\n", argv[i]);
-        //storeToMemory(argv[i]);
+        printf("%s\n", argv[i]);
+        storeToMemory(argv[i]);
        // execute(argv[i], i);
     }
     
